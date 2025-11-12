8229618b11a7b4bb038d4334c0a211147119ba6a
Albert Yakubov <mrayakubov@hotmail.com>
2025-11-11 20:40:56 -0700

    added gestures for emoji stickers

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -8,8 +8,7 @@ import ImageViewer from '@/components/ImageViewer';
 import * as ImagePicker from 'expo-image-picker';
 import React, { useState } from 'react';
 import { ImageSourcePropType, StyleSheet, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -50,7 +49,7 @@ export default function Index() {
   }
 
   return (
-    <SafeAreaView style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
           {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
@@ -73,7 +72,7 @@ export default function Index() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-    </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
